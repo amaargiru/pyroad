0c36651297db0cc3069506472b77a75df82c42a1
amaargiru <war4one@gmail.com>
2022-11-09 16:22:59 +0500

    Rename file from 'SQL' to 'Database'

--- compose_readme.bat
+++ compose_readme.bat
@@ -1,3 +1,3 @@
-copy 00_intro.md + 01_Data_Structures.md + 02_Data_Management.md + 03_Data_Flows.md + 04_OOP.md + 05_Language_Skeleton.md + 06_Multithreading_Multiprocessing.md + 07_Common_Practice.md + 08_Algorithms.md + 09_SQL.md + 10_Net.md + 11_Architecture.md + 12_DevOps.md + 13_Full.md + 14_outro.md readme.md
+copy 00_intro.md + 01_Data_Structures.md + 02_Data_Management.md + 03_Data_Flows.md + 04_OOP.md + 05_Language_Skeleton.md + 06_Multithreading_Multiprocessing.md + 07_Common_Practice.md + 08_Algorithms.md + 09_Database.md + 10_Net.md + 11_Architecture.md + 12_DevOps.md + 13_Full.md + 14_outro.md readme.md
 
 pause
